17d914a688ef8f94bf07236bc27c9b114bc52b69
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-01-27 02:56:52 +0500

    Add ElonsToys

---
+++ ExercismCSharpLib/ElonsToys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercismCSharpLib;
+public class ElonsToys
+{
+    private int meters;
+    private byte battery = 100;
+
+    public static ElonsToys Buy() => new();
+
+    public string DistanceDisplay() => $"Driven {meters} meters";
+
+    public string BatteryDisplay() => battery != 0 ? $"Battery at {battery}%" : "Battery empty";
+
+    public void Drive()
+    {
+        if (battery > 0)
+        {
+            meters += 20;
+            battery--;
+        }
+    }
+}
