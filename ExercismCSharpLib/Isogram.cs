05721ceb58ef159f928bec355ff7cd6adf04e291
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-01-27 13:42:28 +0500

    Add Isogram

---
+++ ExercismCSharpLib/Isogram.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercismCSharpLib;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        List<char> letters = new();
+        foreach (char c in word)
+        {
+            if (char.IsLetter(c))
+            {
+                if (letters.Contains(char.ToLower(c)))
+                    return false;
+                else
+                    letters.Add(char.ToLower(c));
+            }
+        }
+
+        return true;
+    }
+}
