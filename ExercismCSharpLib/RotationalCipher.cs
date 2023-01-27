6c5cfd79df4747cc38d0d0e163f0f1702bbe73af
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-01-27 13:16:13 +0500

    Add RotationalCipher

---
+++ ExercismCSharpLib/RotationalCipher.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExercismCSharpLib;
+
+public static class RotationalCipher
+{
+    public static string Rotate(string text, int shiftKey) =>
+        Regex.Replace(text, @"[a-zA-Z]", c =>
+        {
+            char[] charArray = c.ToString().ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                char d = Char.IsUpper(charArray[i]) ? 'A' : 'a';
+                charArray[i] = (char)((((charArray[i] + shiftKey) - d) % 26) + d);
+            }
+            return new(charArray);
+        });
+
+    #region Hayashida Katsutoshi's Solution
+    //public static string Rotate(string text, int shiftKey)
+    //{
+    //    char Rotate(char c)
+    //    {
+    //        if (!char.IsLetter(c))
+    //            return c;
+    //        int b = char.IsLower(c) ? 'a' : 'A';
+    //        return (char)(b + ((c - b + shiftKey) % 26));
+    //    }
+    //    return new string(text.Select(Rotate).ToArray());
+    //}
+    #endregion
+}
