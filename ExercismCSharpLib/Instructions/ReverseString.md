9c446f7fe53f428ea2c0b4c69df1db1218a09980
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-01-27 14:23:00 +0500

    Add ReverseString

---
+++ ExercismCSharpLib/Instructions/ReverseString.md
@@ -0,0 +1,5 @@
+﻿# Reverse String
+
+Reverse a string
+
+For example: input: "cool" output: "looc"
---
+++ ExercismCSharpLib/ReverseString.cs
@@ -0,0 +1,55 @@
+﻿namespace ExercismCSharpLib;
+
+public class ReverseString
+{
+    #region Approach: Span<T> (best performance)
+    public static string Reverse(string input)
+    {
+        Span<char> chars = new char[input.Length];
+        for (var i = 0; i < input.Length; i++)
+        {
+            chars[input.Length - 1 - i] = input[i];
+        }
+        return new string(chars);
+    }
+    #endregion
+
+    #region Approach: Array.Reverse() (very similar performance to the Span<T>)
+    //public static string Reverse(string input)
+    //{
+    //    char[] charArray = input.ToCharArray();
+    //    Array.Reverse(charArray);
+    //    return new(charArray);
+    //}
+    #endregion
+
+    #region Approach: LINQ (slowest and allocates the most memory)
+    //public static string Reverse(string input)
+    //{
+    //    return new string(input.Reverse().ToArray());
+    //}
+
+    // Approach: StringBuilder
+    //public static string Reverse(string input)
+    //{
+    //    var chars = new StringBuilder();
+    //    for (var i = input.Length - 1; i >= 0; i--)
+    //    {
+    //        chars.Append(input[i]);
+    //    }
+    //    return chars.ToString();
+    //}
+    #endregion
+
+    #region Approach: StringBuilder (slightly better, but still quite a bit slower than the two top-performing approaches)
+    //public static string Reverse(string input)
+    //{
+    //    var chars = new StringBuilder();
+    //    for (var i = input.Length - 1; i >= 0; i--)
+    //    {
+    //        chars.Append(input[i]);
+    //    }
+    //    return chars.ToString();
+    //}
+    #endregion
+}
