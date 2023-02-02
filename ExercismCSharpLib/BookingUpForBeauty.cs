962679bd2d895fab76243794ec473f08be5bce99
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-02-02 23:13:39 +0500

    Add BookingUpForBeauty

---
+++ ExercismCSharpLib/BookingUpForBeauty.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace ExercismCSharpLib;
+
+public static class BookingUpForBeauty
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        if (DateTime.TryParse(appointmentDateDescription, out var date))
+            return date;
+        else if (DateTime.TryParseExact(appointmentDateDescription, "M/d/yyyy HH:mm:ss",
+            new CultureInfo("en-US"), DateTimeStyles.None, out var dateValue))
+            return dateValue;
+
+        throw new FormatException($"String '{appointmentDateDescription}' was not recognized as a valid DateTime");
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        var currentDate = DateTime.Now;
+        int result = DateTime.Compare(currentDate, appointmentDate);
+
+        if (result > 0)
+            return true;
+        
+        return false;
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+        if (appointmentDate.Hour >= 12 && appointmentDate.Hour < 18)
+            return true;
+
+        return false;
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+        return $"You have an appointment on {appointmentDate.ToString(new CultureInfo("en_US"))}.";
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+        return new DateTime(DateTime.Now.Year, 9, 15);
+    }
+}
---
+++ xUnitTest/BookingUpForBeautyTests.cs
@@ -0,0 +1,53 @@
+﻿using ExercismCSharpLib;
+
+namespace xUnitTest;
+
+public static class BookingUpForBeautyTests
+{
+    [Fact]
+    public static void Schedule_Fromat_1()
+    {
+        Assert.Equal(new DateTime(2019, 7, 25, 13, 45, 0), 
+            BookingUpForBeauty.Schedule("7/25/2019 13:45:00"));
+    }
+
+    [Fact]
+    public static void Schedule_Fromat_2()
+    {
+        Assert.Equal(new DateTime(2019, 7, 25, 13, 45, 0),
+            BookingUpForBeauty.Schedule("July 25, 2019 13:45:00"));
+    }
+
+    [Fact]
+    public static void Schedule_Fromat_3()
+    {
+        Assert.Equal(new DateTime(2019, 7, 25, 13, 45, 0),
+            BookingUpForBeauty.Schedule("Thursday, July 25, 2019 13:45:00"));
+    }
+
+    [Fact]
+    public static void HasPassed_Test()
+    {
+        Assert.True(BookingUpForBeauty.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
+    }
+
+    [Fact]
+    public static void IsAfternoonAppointment_Test()
+    {
+        Assert.True(BookingUpForBeauty.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
+    }
+
+    [Fact]
+    public static void Description_Test()
+    {
+        Assert.Equal("You have an appointment on 3/29/2019 3:00:00 PM.",
+            BookingUpForBeauty.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
+    }
+
+    [Fact]
+    public static void AnniversaryDate_Test()
+    {
+        Assert.Equal(new DateTime(DateTime.Now.Year, 9, 15),
+            BookingUpForBeauty.AnniversaryDate());
+    }
+}
