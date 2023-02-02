06fff1515faec4918b7c32bebaa885dd580d22ac
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-02-03 00:30:03 +0500

    Update SumOfMultiples by p4k03n4t0r

--- ExercismCSharpLib/SumOfMultiples.cs
+++ ExercismCSharpLib/SumOfMultiples.cs
@@ -5,18 +5,9 @@ namespace ExercismCSharpLib;
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max) =>
-        Enumerable.Range(1, max -1).Where(i => IsDivide(multiples, i)).Sum();
-
-    private static bool IsDivide(IEnumerable<int> multiples, int i)
-    {
-        foreach (int item in multiples)
-        {
-            if (item == 0)
-                return false;
-            if (i % item == 0)
-                return true;
-        }
-
-        return false;
-    }
+        multiples
+            .SelectMany(multiple => Enumerable.Range(1, max - 1)
+            .Where(n => multiple != 0 && n % multiple == 0))
+            .Distinct()
+            .Sum();
 }
